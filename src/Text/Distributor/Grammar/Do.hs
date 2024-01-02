a7685880efa76c8d96cc5be22cac5191fd2f8ac1
Eitan Chatav <eitan.chatav@gmail.com>
2024-01-01 19:13:06 -0800

    Update Do.hs

--- src/Text/Distributor/Grammar/Do.hs
+++ src/Text/Distributor/Grammar/Do.hs
@@ -15,45 +15,6 @@ import GHC.TypeLits
 import Prelude hiding (return, (>>=))
 import Text.Distributor.Grammar
 
-class FixNT (s :: [Symbol]) p where
-  fixNT :: (p -> p) -> p
-instance FixNT '[] p where
-  fixNT = fix
-instance
-  ( KnownSymbol s
-  , NonTerminal p
-  ) => FixNT '[s] p where
-    fixNT = recNonTerminal (symbolVal' @s proxy#)
-instance
-  ( KnownSymbol s0
-  , NonTerminal p0
-  , KnownSymbol s1
-  , NonTerminal p1
-  ) => FixNT '[s0,s1] (p0,p1) where
-    fixNT f =
-      let
-        ~(p0,p1) =
-            ( fixNT @'[s0] @p0 (\q -> view _1 (f (q,p1)))
-            , fixNT @'[s1] @p1 (\q -> view _2 (f (p0,q)))
-            )
-      in (p0,p1)
-instance
-  ( KnownSymbol s0
-  , NonTerminal p0
-  , KnownSymbol s1
-  , NonTerminal p1
-  , KnownSymbol s2
-  , NonTerminal p2
-  ) => FixNT '[s0,s1,s2] (p0,p1,p2) where
-    fixNT f =
-      let
-        ~(p0,p1,p2) =
-            ( fixNT @'[s0] @p0 (\q -> view _1 (f (q,p1,p2)))
-            , fixNT @'[s1] @p1 (\q -> view _2 (f (p0,q,p2)))
-            , fixNT @'[s2] @p2 (\q -> view _3 (f (p0,p1,q)))
-            )
-      in (p0,p1,p2)
-
 data NT (s :: [Symbol]) a where
   NT :: FixNT s a => {getNT :: a} -> NT s a
 instance ('[s0] ~ s1, a0 ~ a1, KnownSymbol s0, NonTerminal a1)
@@ -70,3 +31,36 @@ mfix aF = NT (fixNT @s (getNT . aF))
 
 (>>=) :: forall s t a b. FixNT s a => NT s a -> (a -> NT t b) -> NT t b
 a >>= f = f (fixNT @s (const (getNT a)))
+
+class FixNT (s :: [Symbol]) p where
+  fixNT :: (p -> p) -> p
+instance FixNT '[] p where
+  fixNT = fix
+instance
+  ( KnownSymbol s, NonTerminal p
+  ) => FixNT '[s] p where
+    fixNT = recNonTerminal (symbolVal' @s proxy#)
+instance
+  ( KnownSymbol s0, NonTerminal p0
+  , KnownSymbol s1, NonTerminal p1
+  ) => FixNT '[s0,s1] (p0,p1) where
+    fixNT f =
+      let
+        ~(p0,p1) =
+            ( fixNT @'[s0] @p0 (\q -> view _1 (f (q,p1)))
+            , fixNT @'[s1] @p1 (\q -> view _2 (f (p0,q)))
+            )
+      in (p0,p1)
+instance
+  ( KnownSymbol s0, NonTerminal p0
+  , KnownSymbol s1, NonTerminal p1
+  , KnownSymbol s2, NonTerminal p2
+  ) => FixNT '[s0,s1,s2] (p0,p1,p2) where
+    fixNT f =
+      let
+        ~(p0,p1,p2) =
+            ( fixNT @'[s0] @p0 (\q -> view _1 (f (q,p1,p2)))
+            , fixNT @'[s1] @p1 (\q -> view _2 (f (p0,q,p2)))
+            , fixNT @'[s2] @p2 (\q -> view _3 (f (p0,p1,q)))
+            )
+      in (p0,p1,p2)
