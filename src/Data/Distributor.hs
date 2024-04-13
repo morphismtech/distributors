c3f619d09e9738179a57be9b4bd72279b5da54f2
Eitan Chatav <eitan.chatav@gmail.com>
2024-04-12 21:19:05 -0700

    free data

--- src/Data/Distributor.hs
+++ src/Data/Distributor.hs
@@ -37,12 +37,16 @@ module Data.Distributor
   ( -- * lax monoidal profunctors
     Monoidal (oneP, (>*<)), dimap2, (>*), (*<), (>:<)
   , pureP, apP, liftA2P, replicateP, replicateP', replicateP_, foreverP
+  , Mon (Mon), liftMon
+  , ChooseMon, liftChooseMon, foldChooseMon
     -- * traversal
   , wanderP, traverseP, traversalP
     -- * lax distributive profunctors
   , Distributor (zeroP, (>+<), several, severalMore, possibly)
-  , dialt, several1
-  , atLeast0, moreThan0, atLeast1
+  , dialt
+  , Dist (DistZero, DistPlus), liftDist
+  , DistAlt (DistAlts), liftDistAlt
+  , several1, atLeast0, moreThan0, atLeast1
   , sep, Separate (by, beginBy, endBy)
     -- * pattern matching
   , eot, onCase, onCocase
@@ -56,6 +60,7 @@ import Control.Lens hiding (chosen, Traversing)
 import Control.Lens.Internal.Context
 import Control.Lens.PartialIso
 import Control.Lens.Stream
+import Control.Monad
 import Data.Bifunctor.Biff
 import Data.Bifunctor.Clown
 import Data.Bifunctor.Joker
@@ -73,6 +78,7 @@ import Data.Profunctor.Yoneda
 import Data.Void
 import Generics.Eot
 import GHC.Base (Constraint, Type)
+import Witherable
 
 {- | A lax monoidal profunctor with respect to product
 or simply a product profunctor
@@ -249,6 +255,109 @@ replicateP_ n p = p >* replicateP_ (n-1) p
 
 foreverP :: Monoidal p => p () c -> p a b
 foreverP p = let p' = p >* p' in p'
+
+type Mon
+  :: ((Type -> Type) -> (Type -> Type))
+  -- ^ your choice of free applicative
+  -> (Type -> Type -> Type) 
+  -> Type -> Type -> Type
+liftMon :: ap (p a) b -> Mon ap p a b
+liftMon = Mon id
+data Mon ap p a b where
+  Mon :: (a -> s) -> ap (p s) b -> Mon ap p a b
+instance (forall f. Functor (ap f))
+  => Functor (Mon ap p a) where
+    fmap g (Mon f x) = Mon f (g <$> x)
+instance (forall f. Applicative (ap f))
+  => Applicative (Mon ap p a) where
+    pure b = liftMon (pure b)
+    Mon f0 x0 <*> Mon f1 x1 =
+      lmap f0 (liftMon x0) <*> lmap f1 (liftMon x1)
+instance (forall f. Functor (ap f)) => Profunctor (Mon ap p) where
+  dimap h g (Mon f x) = Mon (f . h) (g <$> x)
+instance (forall f. Applicative (ap f)) => Monoidal (Mon ap p) where
+  oneP = Mon id (pure ())
+  Mon f0 x0 >*< Mon f1 x1 =
+    lmap (f0 *** f1) (liftMon x0 >*< liftMon x1)
+instance (forall f. Filterable (ap f)) => Filterable (Mon ap p x) where
+  catMaybes (Mon f a) = Mon f (catMaybes a)
+instance (forall f. Filterable (ap f)) => Cochoice (Mon ap p) where
+  unleft (Mon f a) = Mon (f . Left) (mapMaybe (either Just (const Nothing)) a)
+  unright (Mon f a) = Mon (f . Right) (mapMaybe (either (const Nothing) Just) a)
+
+data ChooseMon p a b where
+  ChoosePure :: Maybe b -> ChooseMon p a b
+  ChooseAp
+    :: (a -> Maybe s)
+    -> ChooseMon p a (t -> Maybe b)
+    -> p s t
+    -> ChooseMon p a b
+instance Functor (ChooseMon p a) where fmap = rmap
+instance Filterable (ChooseMon p a) where
+  mapMaybe = mapMaybeP
+instance Applicative (ChooseMon p a) where
+  pure = ChoosePure . Just
+  ChoosePure Nothing <*> _ = ChoosePure Nothing
+  ChoosePure (Just f) <*> x = f <$> x
+  ChooseAp f g x <*> y =
+    let
+      apply h a t = ($ a) <$> h t
+    in
+      ChooseAp f (apply <$> g <*> y) x
+instance Profunctor (ChooseMon p) where
+  dimap _ g (ChoosePure b) = ChoosePure (g <$> b)
+  dimap f' g' (ChooseAp f g x) =
+    ChooseAp (f . f') ((fmap g' .) <$> lmap f' g) x
+instance Monoidal (ChooseMon p)
+instance Choice (ChooseMon p) where
+  left' (ChoosePure b) = ChoosePure (Left <$> b)
+  left' (ChooseAp f g x) =
+    let
+      apply e t = either ((Left <$>) . ($ t)) (Just . Right) e
+    in
+      ChooseAp (either f (pure Nothing)) (apply <$> (left' g)) x
+  right' (ChoosePure b) = ChoosePure (Right <$> b)
+  right' (ChooseAp f g x) =
+    let
+      apply e t = either (Just . Left) ((Right <$>) . ($ t)) e
+    in
+      ChooseAp (either (pure Nothing) f) (apply <$> (right' g)) x
+instance Cochoice (ChooseMon p) where
+  unleft (ChoosePure b) =
+    ChoosePure (either Just (pure Nothing) =<< b)
+  unleft (ChooseAp f g x) =
+    let
+      g' = (Left . (either Just (pure Nothing) <=<)) <$> g
+    in
+      ChooseAp (f . Left) (unleft g') x
+  unright (ChoosePure b) =
+    ChoosePure (either (pure Nothing) Just =<< b)
+  unright (ChooseAp f g x) =
+    let
+      g' = (Right . (either (pure Nothing) Just <=<)) <$> g
+    in
+      ChooseAp (f . Right) (unright g') x
+instance ProfunctorFunctor ChooseMon where
+  promap _ (ChoosePure b) = ChoosePure b
+  promap h (ChooseAp f g x) = ChooseAp f (promap h g) (h x)
+
+foldChooseMon
+  :: (Monoidal q, Choice q, Cochoice q)
+  => (forall x y. p x y -> q x y)
+  -> ChooseMon p a b
+  -> q a b
+foldChooseMon k = \case
+  ChoosePure Nothing -> catMaybesP (pureP Nothing)
+  ChoosePure (Just b) -> pureP b
+  ChooseAp f g x ->
+    let
+      h = foldChooseMon k g
+      y = dimapMaybe f Just (k x)
+    in
+      catMaybesP (liftA2P ($) h y)
+
+liftChooseMon :: p a b -> ChooseMon p a b
+liftChooseMon = ChooseAp Just (pure Just)
 
 newtype FunList a b t = FunList {unFunList :: Either t (a, Bazaar (->) a b (b -> t))}
 
@@ -478,6 +587,99 @@ dialt
   -> (d -> t)
   -> p a b -> p c d -> p s t
 dialt f g h p q = dimap f (either g h) (p >+< q)
+type Dist
+  :: ((Type -> Type) -> (Type -> Type))
+  -- ^ your choice of free applicative
+  -> (Type -> Type -> Type) 
+  -> Type -> Type -> Type
+data Dist ap p a b where
+  DistZero 
+    :: (a -> Void)
+    -> Dist ap p a b
+  DistPlus
+    :: (a -> Either s c)
+    -> ap (p s) b
+    -> Dist ap p c b
+    -> Dist ap p a b
+liftDist :: ap (p a) b -> Dist ap p a b
+liftDist x = DistPlus Left x (DistZero id)
+instance (forall f. Functor (ap f))
+  => Functor (Dist ap p a) where fmap = rmap
+instance (forall f. Applicative (ap f))
+  => Applicative (Dist ap p a) where
+  pure b = liftDist (pure b)
+  -- 0*x=0
+  liftA2 _ (DistZero absurdum) _ = DistZero absurdum
+  -- (x+y)*z=x*z+y*z
+  liftA2 g (DistPlus f x y) z =
+    let
+      ff a = bimap (,a) (,a) (f a)
+    in
+      dialt ff id id
+        (uncurry g <$> (liftDist x >*< z))
+        (uncurry g <$> (y >*< z))
+instance (forall f. Functor (ap f))
+  => Profunctor (Dist ap p) where
+  dimap f _ (DistZero absurdum) = DistZero (absurdum . f)
+  dimap f' g' (DistPlus f x y) =
+    DistPlus (f . f') (g' <$> x) (g' <$> y)
+instance (forall f. Applicative (ap f)) => Monoidal (Dist ap p)
+instance (forall f. Applicative (ap f))
+  => Distributor (Dist ap p) where
+  zeroP = DistZero absurd
+  -- 0+x=x
+  DistZero absurdum >+< x =
+    dimap (either (absurd . absurdum) id) Right x
+  -- (x+y)+z=x+(y+z)
+  DistPlus f x y >+< z =
+    let
+      assocE (Left (Left a)) = Left a
+      assocE (Left (Right b)) = Right (Left b)
+      assocE (Right c) = Right (Right c)
+      f' = assocE . either (Left . f) Right
+    in
+      dialt f' Left id (liftDist x) (y >+< z)
+instance (forall f. Filterable (ap f)) => Filterable (Dist ap p x) where
+  catMaybes (DistZero absurdum) = DistZero absurdum
+  catMaybes (DistPlus f x y) = DistPlus f (catMaybes x) (catMaybes y)
+instance (forall f. Filterable (ap f)) => Cochoice (Dist ap p) where
+  unleft (DistZero absurdum) = DistZero (absurdum . Left)
+  unleft (DistPlus f x y) =
+    DistPlus (f . Left)
+      (mapMaybe (either Just (const Nothing)) x)
+      (mapMaybe (either Just (const Nothing)) y)
+  unright (DistZero absurdum) = DistZero (absurdum . Right)
+  unright (DistPlus f x y) =
+    DistPlus (f . Right)
+      (mapMaybe (either (const Nothing) Just) x)
+      (mapMaybe (either (const Nothing) Just) y)
+newtype DistAlt p a b = DistAlts [p a b]
+instance (forall x. Functor (p x)) => Functor (DistAlt p a) where
+  fmap f (DistAlts alts) = DistAlts (map (fmap f) alts)
+instance (forall x. Applicative (p x))
+  => Applicative (DistAlt p a) where
+  pure b = liftDistAlt (pure b)
+  DistAlts xs <*> DistAlts ys =
+    DistAlts [x <*> y | x <- xs, y <- ys]
+instance (forall x. Applicative (p x))
+  => Alternative (DistAlt p a) where
+    empty = DistAlts []
+    DistAlts altsL <|> DistAlts altsR = DistAlts (altsL ++ altsR)
+instance Profunctor p => Profunctor (DistAlt p) where
+  dimap f g (DistAlts alts) = DistAlts (map (dimap f g) alts)
+instance (forall x. Applicative (p x), Profunctor p)
+  => Monoidal (DistAlt p)
+instance Choice p => Choice (DistAlt p) where
+  left' (DistAlts alts) = DistAlts (map left' alts)
+  right' (DistAlts alts) = DistAlts (map right' alts)
+instance Cochoice p => Cochoice (DistAlt p) where
+  unleft (DistAlts alts) = DistAlts (map unleft alts)
+  unright (DistAlts alts) = DistAlts (map unright alts)
+instance (Choice p, Cochoice p, forall x. Applicative (p x))
+  => Distributor (DistAlt p)
+
+liftDistAlt :: p a b -> DistAlt p a b
+liftDistAlt p = DistAlts [p]
 
 -- positional pattern matching
 eot
