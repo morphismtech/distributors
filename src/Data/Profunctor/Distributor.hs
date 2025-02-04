2c9c052b4fb8a4d24cd11bd555a7a62cb6765403
Eitan Chatav <eitan.chatav@gmail.com>
2025-02-03 17:47:32 -0800

    Update Distributor.hs

--- src/Data/Profunctor/Distributor.hs
+++ src/Data/Profunctor/Distributor.hs
@@ -157,7 +157,7 @@ atLeast1
 atLeast1 sep p = dimap unlist (either list0 list1)
   (right' (beginBy sep >* p >*< manyP (separateBy sep >* p) *< endBy sep))
 
-{- | Used to parse multiple times, delimited `by` a separateBy,
+{- | Used to parse multiple times, delimited `by` a `separateBy`,
 a `beginBy`, and an `endBy`. -}
 data SepBy p = SepBy
   { beginBy :: p () ()
