5eb59e4f763af18a0f1e4954b5fae24110988b0a
Eitan Chatav <eitan.chatav@gmail.com>
2025-03-16 16:40:16 -0700

    Update Wither.hs

--- src/Control/Lens/Wither.hs
+++ src/Control/Lens/Wither.hs
@@ -12,16 +12,18 @@ See Chris Penner,
 -}
 
 module Control.Lens.Wither
-  ( Wither
-  , Wither'
+  ( -- * Wither
+    Wither
   , AWither
-  , AWither'
-  , Witheroid
+    -- * Combinators
   , cloneWither
   , withered
   , filtraversed
   , filterOf
+    -- * Witheroid
+  , Witheroid
   , witherPrism
+    -- * Altar
   , Altar (..)
   ) where
 
@@ -33,38 +35,64 @@ import Data.Profunctor.Sieve
 import Prelude hiding (filter)
 import Witherable
 
+{- | `Wither`s extends `Control.Lens.Traversal.Traversal`s by filtering.
+
+
+Every one of the following is a `Wither`.
+
+* `Control.Lens.Iso.Iso`
+* `Control.Lens.Lens.Lens`
+* `Control.Lens.Prism.Prism`
+* `Control.Lens.Traversal.Traversal`
+* `Witheroid`
+-}
 type Wither s t a b = forall f. Alternative f => (a -> f b) -> s -> f t
 
-type Wither' s a = Wither s s a a
-
+{- | If you see `AWither` in a signature for a function,
+the function is expecting a `Wither`. -}
 type AWither s t a b = (a -> Altar a b b) -> s -> Altar a b t
 
-type AWither' s a = AWither s s a a
-
+{- | `Witheroid`s generalize `Wither`s.
+Every `Control.Lens.Prism.Prism` is a `Witheroid`.
+-}
 type Witheroid s t a b = forall p f.
   (Choice p, Alternative f)
     => p a (f b) -> p s (f t)
 
+{- | Clone `AWither` so that you can reuse the same
+monomorphically typed `Wither` for different purposes.
+-}
 cloneWither :: AWither s t a b -> Wither s t a b
 cloneWither w f = (\g z -> runAltar z g) f . w sell
 
+{- | Construct a `Wither` for a `Witherable`. -}
 withered :: Witherable t => Wither (t a) (t b) a b
 withered f = wither (optional . f)
 
+{- |
+prop> withered = filtraversed
+-}
 filtraversed :: (Filterable t, Traversable t) => Wither (t a) (t b) a b
 filtraversed f = fmap catMaybes . traverse (optional . f)
 
+{- | Filter a traversed structure based on a predicate. -}
 filterOf :: Alternative m => Wither s t a a -> (a -> Bool) -> s -> m t
 filterOf w p s = w guardingp s where
   guardingp a
     | p a = pure a
     | otherwise = empty
 
+{- |
+`Control.Lens.Prism.Prism`s already capture the idea of success and failure,
+but they simply skip the traversal if the prism doesn't match.
+Lift prisms into withers such that they'll fail in a way that wither can catch.
+-}
 witherPrism :: APrism s t a b -> Witheroid s t a b
 witherPrism prsm =
   withPrism prsm $ \embed match ->
     dimap match (either (const empty) (fmap embed)) . right'
 
+{- | This is used to characterize `Wither`s. -}
 newtype Altar a b t = Altar
   { runAltar :: forall f. Alternative f => (a -> f b) -> f t }
 instance Functor (Altar a b) where
