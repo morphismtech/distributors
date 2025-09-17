0d879b217ac145087d14f3f12bdf7f8c1f73f421
Eitan Chatav <eitan.chatav@gmail.com>
2025-09-16 19:11:20 -0700

    Tokenized?

--- src/Control/Lens/MLens.hs
+++ src/Control/Lens/MLens.hs
@@ -14,24 +14,27 @@ module Control.Lens.MLens
   ) where
 
 import Data.Profunctor
+import Data.Profunctor.Distributor
 import Data.Profunctor.Monadic
 
 type MLens s t a b = forall p f. (Monadic p, Monad f) => p a (f b) -> p s (f t)
 
-data Monastery s t a b = Monastery (s -> (b,s)) (a -> (b, t -> t))
-instance Functor (Monastery s t a) where fmap = rmap
-instance Applicative (Monastery s t a) where
-  pure b = Monastery (\s -> (b,s)) (\_ -> (b, id))
+data Monastery a b s t = Monastery (b -> (t,b)) (s -> (t, a -> a))
+instance Functor (Monastery a b s) where fmap = rmap
+instance Applicative (Monastery a b s) where
+  pure t = Monastery (\a -> (t,a)) (\_ -> (t,id))
   Monastery getf putf <*> Monastery geta puta =
     Monastery
-      (\s -> let (f, s') = getf s; (a, s'') = geta s' in (f a, s''))
-      (\x -> let (f, r) = putf x; (a, r') = puta x in (f a, r' . r))
-instance Monad (Monastery s t a) where
+      (\b -> let (f,b') = getf b; (x,b'') = geta b' in (f x, b''))
+      (\s -> let (f,r) = putf s; (x,r') = puta s in (f x, r' . r))
+instance Monad (Monastery a b s) where
   return = pure
   Monastery get put >>= f = Monastery
-    (\s -> let Monastery get' _ = f (fst (get s)) in get' s)
-    (\x -> let (a,r) = put x; Monastery _ put' = f a; (b, r') = put' x in (b, r' . r))
-instance Profunctor (Monastery s t) where
+    (\b -> let Monastery get' _ = f (fst (get b)) in get' b)
+    (\s -> let (x,r) = put s; Monastery _ put' = f x; (x', r') = put' s in (x', r' . r))
+instance Profunctor (Monastery a b) where
   dimap f g (Monastery get put) = Monastery
     (first' g . get)
     (first' g . put . f)
+instance Tokenized c c (Monastery c c) where
+  anyToken = Monastery (\c -> (c,c)) (\c -> (c,id))
