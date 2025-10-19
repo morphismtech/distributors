fab5a95fd18d4eaf3b441d47e726db47412dc023
Eitan Chatav <eitan.chatav@gmail.com>
2025-10-19 06:45:32 -0700

    Update Equator.hs

--- src/Control/Lens/Internal/Equator.hs
+++ src/Control/Lens/Internal/Equator.hs
@@ -1,8 +1,21 @@
 module Control.Lens.Internal.Equator
   ( -- *
-    -- Equator
+    Equator (..)
   ) where
 
+import Control.Lens
+import Control.Lens.Internal.Iso
+import Control.Lens.Internal.Prism
+import Control.Lens.Internal.Profunctor
 
--- class Equator a b p | p -> a, p -> b where equate :: p a b
--- instance Equator a b (Identical a b) where equate = Identical
+class Equator a b p | p -> a, p -> b where equate :: p a b
+instance Equator a b (Identical a b) where equate = Identical
+instance Equator a b (Exchange a b) where
+  equate = Exchange id id
+instance Equator a b (Market a b) where
+  equate = Market id Right
+-- instance Equator a b (PartialExchange a b) where
+--   equate = PartialExchange Just Just
+instance (Equator a b p, Profunctor p, Applicative f)
+  => Equator a b (WrappedPafb f p) where
+    equate = WrapPafb (rmap pure equate)
