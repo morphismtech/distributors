c6db189b4132474e4ba091dba266e1569aa0541e
Eitan Chatav <eitan.chatav@gmail.com>
2025-10-23 16:34:56 -0700

    regexGrammar

--- src/Control/Lens/Grammar.hs
+++ src/Control/Lens/Grammar.hs
@@ -3,26 +3,29 @@ module Control.Lens.Grammar
     RegGrammar
   , Grammar
   , CtxGrammar
+  , Grammarr
   , Gram (..)
   , genRegEx
   , genGram
   , genShowS
   , genReadS
-  , Grammatical (..)
+  , Rules (..)
   , Regular
-  , Grammaticator
+  , Grammatical
   , Contextual
   , NonTerminalSymbol (..)
+  , regexGrammar
   ) where
 
 import Control.Applicative
+import Control.Lens
+import Control.Lens.PartialIso
 import Control.Lens.RegEx
 import Control.Lens.Token
 import Control.Lens.Stream
 import Control.Monad
 import Data.Function
 import Data.Monoid
-import Data.Profunctor
 import Data.Profunctor.Distributor
 import Data.Profunctor.Monadic
 import Data.Profunctor.Syntax
@@ -32,19 +35,23 @@ import Type.Reflection
 import Witherable
 
 type RegGrammar c a = forall p. Regular c p => p a a
-type Grammar c a = forall p. Grammaticator c p => p a a
+type Grammar c a = forall p. Grammatical c p => p a a
 type CtxGrammar s a = forall p m. Contextual s m p => p s s m a a
+
+type Grammarr c a b = forall p. Grammatical c p => p a a -> p b b
 
 data Gram c = Gram
   { startGram :: (All, RegEx c)
   , rulesGram :: Set (String, (All, RegEx c))
   }
+deriving stock instance
+  (Show c, Categorized c, Show (Categorize c)) => Show (Gram c)
 
 genGram
   :: (Categorized c, Ord c, Ord (Categorize c))
   => Grammar c a
   -> Gram c
-genGram gram = case runInvariantP gram of (rules, start) -> Gram start rules
+genGram = (\(rules, start) -> Gram start rules) . runInvariantP
 
 genRegEx :: Categorized c => RegGrammar c a -> RegEx c
 genRegEx = runInvariantP
@@ -63,28 +70,29 @@ type Regular c p =
   , Alternator p
   )
 
-type Grammaticator c p =
+type Grammatical c p =
   ( Regular c p
   , Filtrator p
-  , forall x. Grammatical (p x x)
+  , forall x. Rules (p x x)
   )
 
 type Contextual s m p =
-  ( IsStream s, Grammaticator (Item s) (p s s m)
-  , Alternative m, Filterable m, MonadPlus m
-  , Polyadic p, Tetradic m p
+  ( Subtextual s m
+  , Grammatical (Item s) (p s s m)
+  , Polyadic p
+  , Tetradic m p
   )
 
-class Grammatical a where
+class Rules a where
   rule :: String -> a -> a
   rule _ = id
   ruleRec :: String -> (a -> a) -> a
   ruleRec _ = fix
-instance Grammatical (Parsor s t m a b)
-instance Grammatical (Printor s t m a b)
-instance Grammatical (Lintor s t m a b)
+instance Rules (Parsor s t m a b)
+instance Rules (Printor s t m a b)
+instance Rules (Lintor s t m a b)
 instance (NonTerminalSymbol a, Ord a)
-  => Grammatical (Set (String, a), a) where
+  => Rules (Set (String, a), a) where
     rule name = ruleRec name . const
     ruleRec name f =
       let
@@ -93,7 +101,7 @@ instance (NonTerminalSymbol a, Ord a)
         rules = insert (name, newRule) oldRules
       in
         (rules, start)
-instance Grammatical p => Grammatical (InvariantP p a b) where
+instance Rules p => Rules (InvariantP p a b) where
   rule name = InvariantP . rule name . runInvariantP
   ruleRec name
     = InvariantP
@@ -114,3 +122,109 @@ instance NonTerminalSymbol (RegEx c) where
 instance (Monoid a, NonTerminalSymbol b)
   => NonTerminalSymbol (a,b) where
     nonTerminal = pure . nonTerminal
+
+makeNestedPrisms ''RegEx
+makeNestedPrisms ''GeneralCategory
+
+regexGrammar :: Grammar Char (RegEx Char)
+regexGrammar = ruleRec "regex" $ \rex -> altG rex
+  where
+
+    altG rex = rule "alternate" $
+      chain1 Left _Alternate (sepBy (terminal "|")) (seqG rex)
+
+    anyG = rule "any" $ _AnyToken >?< terminal "."
+
+    atomG rex = rule "atom" $
+      nonterminalG
+      <|> failG
+      <|> classInG
+      <|> classNotInG
+      <|> categoryInG
+      <|> categoryNotInG
+      <|> _Terminal >?< charG >:< pure ""
+      <|> anyG
+      <|> parenG rex
+
+    categoryG = rule "category" $
+      _LowercaseLetter >?< terminal "Ll"
+      <|> _UppercaseLetter >?< terminal "Lu"
+      <|> _TitlecaseLetter >?< terminal "Lt"
+      <|> _ModifierLetter >?< terminal "Lm"
+      <|> _OtherLetter >?< terminal "Lo"
+      <|> _NonSpacingMark >?< terminal "Mn"
+      <|> _SpacingCombiningMark >?< terminal "Mc"
+      <|> _EnclosingMark >?< terminal "Me"
+      <|> _DecimalNumber >?< terminal "Nd"
+      <|> _LetterNumber >?< terminal "Nl"
+      <|> _OtherNumber >?< terminal "No"
+      <|> _ConnectorPunctuation >?< terminal "Pc"
+      <|> _DashPunctuation >?< terminal "Pd"
+      <|> _OpenPunctuation >?< terminal "Ps"
+      <|> _ClosePunctuation >?< terminal "Pe"
+      <|> _InitialQuote >?< terminal "Pi"
+      <|> _FinalQuote >?< terminal "Pf"
+      <|> _OtherPunctuation >?< terminal "Po"
+      <|> _MathSymbol >?< terminal "Sm"
+      <|> _CurrencySymbol >?< terminal "Sc"
+      <|> _ModifierSymbol >?< terminal "Sk"
+      <|> _OtherSymbol >?< terminal "So"
+      <|> _Space >?< terminal "Zs"
+      <|> _LineSeparator >?< terminal "Zl"
+      <|> _ParagraphSeparator >?< terminal "Zp"
+      <|> _Control >?< terminal "Cc"
+      <|> _Format >?< terminal "Cf"
+      <|> _Surrogate >?< terminal "Cs"
+      <|> _PrivateUse >?< terminal "Co"
+      <|> _NotAssigned >?< terminal "Cn"
+
+    categoryInG = rule "category-in" $
+      _InCategory >?< terminal "\\p{" >* categoryG *< terminal "}"
+
+    categoryNotInG = rule "category-not-in" $
+      _NotInCategory >?< terminal "\\P{" >* categoryG *< terminal "}"
+
+    charG = rule "char" $ charLiteralG <|> charEscapedG
+
+    charEscapedG = rule "char-escaped" $ terminal "\\" >* inClass charsReserved
+
+    charLiteralG = rule "char-literal" $ notInClass charsReserved
+
+    charsReserved = "$()*+.?[\\]^{|}"
+
+    classInG = rule "class-in" $
+      _InClass >?< terminal "[" >* manyP charG *< terminal "]"
+
+    classNotInG = rule "class-not-in" $
+      _NotInClass >?< terminal "[^" >* manyP charG *< terminal "]"
+
+    exprG rex = rule "expression" $
+      terminalG
+      <|> kleeneOptG rex
+      <|> kleeneStarG rex
+      <|> kleenePlusG rex
+      <|> atomG rex
+
+    failG = rule "fail" $ _Fail >?< terminal "\\q"
+
+    nonterminalG = rule "nonterminal" $
+      _NonTerminal >?< terminal "\\q{" >* manyP charG *< terminal "}"
+
+    parenG :: Grammarr Char x x
+    parenG ex = rule "parenthesized" $
+      terminal "(" >* ex *< terminal ")"
+
+    kleeneOptG rex = rule "kleene-optional" $
+      _KleeneOpt >?< atomG rex *< terminal "?"
+
+    kleeneStarG rex = rule "kleene-star" $
+      _KleeneStar >?< atomG rex *< terminal "*"
+
+    kleenePlusG rex = rule "kleene-plus" $
+      _KleenePlus >?< atomG rex *< terminal "+"
+
+    seqG rex = rule "sequence" $
+      chain Left _Sequence (_Terminal . _Empty) noSep (exprG rex)
+
+    terminalG = rule "terminal" $
+      _Terminal >?< someP charG
